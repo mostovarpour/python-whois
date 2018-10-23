02661338ad084a1408c954ae5cff9cfc94e35a01
mostovarpour <matt.ostovarpour@gmail.com>
2018-10-23 13:21:51 -0700

    Modified the deploy script to add the test.com zone.

--- deploy.sh
+++ deploy.sh
@@ -1,80 +1,103 @@
 #!/bin/bash
 
-if [[ "$UID" -ne 0 ]]; then
-  echo "Sorry, you need to run this as root"
-  exit 1
-fi
+while getopts ":i :u :m" opt; do
+  case $opt in
+    i)
+        if [[ "$UID" -ne 0 ]]; then
+        echo "Sorry, you need to run this as root"
+        exit 1
+        fi
 
-lsof -i :53 > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo "It looks like another software is listnening on port 53:"
-  echo ""
-  lsof -i :53
-  echo ""
-  echo "Please disable or uninstall it before installing unbound."
-  while [[ $CONTINUE != "y" && $CONTINUE != "n" ]]; do
-    read -rp "Do you still want to run the script? Unbound might not work... [y/n]: " -e CONTINUE
-  done
-  if [[ "$CONTINUE" = "n" ]]; then
-    exit 2
-  fi
-fi
+        lsof -i :53 > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+        echo "It looks like another software is listnening on port 53:"
+        echo ""
+        lsof -i :53
+        echo ""
+        echo "Please disable or uninstall it before installing unbound."
+        while [[ $CONTINUE != "y" && $CONTINUE != "n" ]]; do
+            read -rp "Do you still want to run the script? Unbound might not work... [y/n]: " -e CONTINUE
+        done
+        if [[ "$CONTINUE" = "n" ]]; then
+            exit 2
+        fi
+        fi
 
-if [[ -e /etc/centos-release || -e /etc/redhat-release || -e /etc/system-release && ! -e /etc/fedora-release ]]; then
-  OS="centos"
-else
-  echo "Looks like you aren't running this installer on CentOS"
-  exit 3
-fi
+        if [[ -e /etc/centos-release || -e /etc/redhat-release || -e /etc/system-release && ! -e /etc/fedora-release ]]; then
+        OS="centos"
+        else
+        echo "Looks like you aren't running this installer on CentOS"
+        exit 3
+        fi
 
-echo ""
-echo "Welcome! This script will install and configure Unbound, and set it as your default system DNS resolver."
-echo ""
-read -n1 -r -p "Press any key to continue..."
-echo ""
+        echo ""
+        echo "Welcome! This script will install and configure Unbound, and set it as your default system DNS resolver."
+        echo ""
+        read -n1 -r -p "Press any key to continue..."
+        echo ""
 
-if [[ "$OS" = "centos" ]]; then
-  # Install Unbound
-  yum install -y unbound
+        # Install Unbound
+        yum install -y unbound
 
-  # Configuration
-  sed -i 's|# interface: 0.0.0.0$|interface: 127.0.0.1|' /etc/unbound/unbound.conf
-  sed -i 's|# hide-identity: no|hide-identity: yes|' /etc/unbound/unbound.conf
-  sed -i 's|# hide-version: no|hide-version: yes|' /etc/unbound/unbound.conf
-  sed -i 's|use-caps-for-id: no|use-caps-for-id: yes|' /etc/unbound/unbound.conf
-fi
+        # Configuration
+        sed -i 's|# interface: 0.0.0.0$|interface: 127.0.0.1|' /etc/unbound/unbound.conf
+        sed -i 's|# hide-identity: no|hide-identity: yes|' /etc/unbound/unbound.conf
+        sed -i 's|# hide-version: no|hide-version: yes|' /etc/unbound/unbound.conf
+        sed -i 's|use-caps-for-id: no|use-caps-for-id: yes|' /etc/unbound/unbound.conf
 
-if [[ ! "$OS" =~ (fedora|centos) ]];then
-  # DNS Rebinding fix
-  echo "private-address: 10.0.0.0/8
-private-address: 172.16.0.0/12
-private-address: 192.168.0.0/16
-private-address: 169.254.0.0/16
-private-address: fd00::/8
-private-address: fe80::/10
-private-address: 127.0.0.0/8
-private-address: ::ffff:0:0/96" >> /etc/unbound/unbound.conf
-fi
+        # DNS Rebinding fix
+        echo "private-address: 10.0.0.0/8
+            private-address: 172.16.0.0/12
+            private-address: 192.168.0.0/16
+            private-address: 169.254.0.0/16
+            private-address: fd00::/8
+            private-address: fe80::/10
+            private-address: 127.0.0.0/8
+            private-address: ::ffff:0:0/96" >> /etc/unbound/unbound.conf
+        
+        # enable our test.com zone 
+        echo "# Custom test.com zone configured below" >> /etc/unbound/unbound.conf
+        echo "local-zone: \"test.com\" static" >> /etc/unbound/unbound.conf
 
-if pgrep systemd-journal; then
-  systemctl enable unbound
-  systemctl restart unbound
-else
-  service unbound restart
-fi
+        #Add the 1000 A records
+        for i in `seq 1 10000`; do
+            echo "local-data: \"r"$i".test.com IN A 127.0.0.1" >> /etc/unbound/unbound.conf
+        done
 
-# Allow the modification of the file
-chattr -i /etc/resolv.conf
+        if pgrep systemd-journal; then
+        systemctl enable unbound
+        systemctl restart unbound
+        else
+        service unbound restart
+        fi
 
-# Disable previous DNS servers
-sed -i "s|nameserver|#nameserver|" /etc/resolv.conf
-sed -i "s|search|#search|" /etc/resolv.conf
+        # Allow the modification of the file
+        chattr -i /etc/resolv.conf
 
-# Set localhost as the DNS resolver
-echo "nameserver 127.0.0.1" >> /etc/resolv.conf
+        # Disable previous DNS servers
+        sed -i "s|nameserver|#nameserver|" /etc/resolv.conf
+        sed -i "s|search|#search|" /etc/resolv.conf
 
-# Disallow the modification to prevent the file from being overwritten by the system.
-# Use -i to enable modifications
-chattr +i /etc/resolv.conf
+        # Set localhost as the DNS resolver
+        echo "nameserver 127.0.0.1" >> /etc/resolv.conf
 
-echo "The installation is done."
+        # Disallow the modification to prevent the file from being overwritten by the system.
+        # Use -i to enable modifications
+        chattr +i /etc/resolv.conf
+
+        echo "The installation is done."
+      ;;
+    u)
+        #Add the 100 A records if the -u flag was given
+        for i in `seq 1001 1100`; do
+            echo "local-data: \"r"$i".test.com IN A 127.0.0.1" >> /etc/unbound/unbound.conf
+        done
+      ;;
+    m)
+      echo "-m was triggered!" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
