4d5f88f6cf6aaad4d7f8668da3c80c50febf94b0
mostovarpour <matt.ostovarpour@gmail.com>
2018-10-23 10:28:38 -0700

    Removed some test files that are not being used anymore.

--- iptest.py
+++
@@ -1,9 +0,0 @@
-import re
-ip_str = "23.34.45.45"
-def ipFormatChk(ip_str):
-   pattern = r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
-   if re.match(pattern, ip_str):
-      return True
-   else:
-      return False
-print ipFormatChk(ip_str)
